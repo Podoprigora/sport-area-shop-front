dff938f14a8151cb77fe661fb3b329a23330b16f
Alexandr Podoprigora <podoprigoraisv@gmail.com>
2020-02-14 19:40:55 +0200

    Refactoring main layout  markup

---
+++ src/components/SvgIcons/feather/LoginIcon.js
@@ -0,0 +1,6 @@
+import Login from '@resources/icons/icomoon-feather/log-in.svg';
+import withIconAttributes from '../withSvgAttrStyles';
+
+const LoginIcon = withIconAttributes(Login);
+
+export default LoginIcon;
--- src/pages/components/Header/Header.js
+++ src/pages/components/Header/Header.js
@@ -4,8 +4,11 @@ import PropTypes from 'prop-types';
 import HeartIcon from '@svg-icons/feather/HeartIcon';
 import ShoppingCartIcon from '@svg-icons/feather/ShoppingCartIcon';
 
+import Link from '@components/Link';
 import Logo from '@resources/images/sport-area-logo.svg';
 import Button from '@components/Button';
+import UserIcon from '@svg-icons/feather/UserIcon';
+import LoginIcon from '@svg-icons/feather/LoginIcon';
 import MenuIcon from '@svg-icons/material/MenuIcon';
 import IconButton from '@components/IconButton';
 import HeaderSearchField from '../HeaderSarchField';
@@ -18,6 +21,14 @@ const Header = (props) => {
                     <a href="#" className="header__logo-link">
                         <img src={Logo} alt="" className="header__logo-img" />
                     </a>
+                    <div className="header__user-container">
+                        <Link primary className="header__link" icon={LoginIcon} iconAlign="left">
+                            Sign In
+                        </Link>
+                        <Link primary className="header__link">
+                            Sing Up
+                        </Link>
+                    </div>
                 </div>
             </div>
             <div className="header__nav-part">
