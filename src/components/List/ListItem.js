cc19767923b3f91f87a6718daa9585ea327d8bfe
Alexandr Podoprigora <podoprigoraisv@gmail.com>
2020-05-18 00:22:53 +0300

    Implemented List's components

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,19 +1,82 @@
-import React from 'react';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import useForkRef from '@components/hooks/useForkRef';
 
 const ListItem = React.forwardRef(function ListItem(props, ref) {
-    const { children, className, alignItems, ...other } = props;
+    const {
+        button,
+        autoFocus,
+        selected,
+        disabled,
+        children,
+        className,
+        alignItems,
+        onClick,
+        onMouseDown,
+        onKeyDown,
+        ...other
+    } = props;
+
+    const innerRef = useRef(null);
+    const handleRef = useForkRef(innerRef, ref);
+
+    const handleClick = useCallback(
+        (ev) => {
+            if (onClick && !disabled) {
+                onClick(ev);
+            }
+        },
+        [onClick, disabled]
+    );
+
+    const handleMouseDown = useCallback(
+        (ev) => {
+            ev.preventDefault();
+
+            if (onMouseDown) {
+                onMouseDown(ev);
+            }
+        },
+        [onMouseDown]
+    );
+
+    const handleKeyDown = useCallback(
+        (ev) => {
+            if (ev.key === 'Enter') {
+                handleClick(ev);
+            }
+
+            if (onKeyDown) {
+                onKeyDown(ev);
+            }
+        },
+        [handleClick, onKeyDown]
+    );
+
+    useEffect(() => {
+        if (autoFocus && button && !disabled) {
+            innerRef.current.focus();
+        }
+    }, [autoFocus, button, disabled]);
+
+    const componentProps = {
+        role: button ? 'button' : 'listitem',
+        tabIndex: button ? 0 : null,
+        className: classNames('list__item', className, {
+            'list__item--button': button,
+            'list__item--selected': selected,
+            'list__item--disabled': disabled,
+            [`u-flex-align-items-${alignItems}`]: alignItems
+        }),
+        ...other,
+        onClick: handleClick,
+        onMouseDown: handleMouseDown,
+        onKeyDown: handleKeyDown
+    };
 
     return (
-        <div
-            role="listitem"
-            className={classNames('list__item', className, {
-                [`u-flex-align-items-${alignItems}`]: alignItems
-            })}
-            ref={ref}
-            {...other}
-        >
+        <div {...componentProps} ref={handleRef}>
             {children}
         </div>
     );
@@ -22,7 +85,14 @@ const ListItem = React.forwardRef(function ListItem(props, ref) {
 ListItem.propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
-    alignItems: PropTypes.oneOf(['flex-start', 'flex-end', 'center', 'stretch', 'baseline'])
+    alignItems: PropTypes.oneOf(['flex-start', 'flex-end', 'center', 'stretch', 'baseline']),
+    button: PropTypes.bool,
+    autoFocus: PropTypes.bool,
+    selected: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onClick: PropTypes.func,
+    onKeyDown: PropTypes.func,
+    onMouseDown: PropTypes.func
 };
 
 export default ListItem;
--- src/pages/TestPage/components/TestList.js
+++ src/pages/TestPage/components/TestList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import List, { ListItem } from '@components/List';
 import ShoppingBasketIcon from '@svg-icons/material/ShoppingBasketIcon';
 import FavoriteOutlineIcon from '@svg-icons/material/FavoriteOutlineIcon';
@@ -13,8 +13,41 @@ import SearchIcon from '@svg-icons/feather/SearchIcon';
 import ListItemIcon from '@components/List/ListItemIcon';
 import ListItemAction from '@components/List/ListItemAction';
 import ListItemText from '@components/List/ListItemText';
+import Input from '@components/Input';
 
 const TestList = () => {
+    const [selectedIndex, setSelectedIndex] = useState(null);
+    const [checked, setChecked] = useState([]);
+
+    const handleItemClick = useCallback(
+        (index) => (ev) => {
+            setSelectedIndex(index);
+        },
+        []
+    );
+
+    const handleChecked = useCallback(
+        (value) => (ev) => {
+            const currentIndex = checked.indexOf(value);
+            const newState = [...checked];
+
+            if (currentIndex !== -1) {
+                newState.splice(currentIndex, 1);
+            } else {
+                newState.push(value);
+            }
+
+            setChecked(newState);
+        },
+        [checked]
+    );
+
+    const handleDeleteClick = (ev) => {
+        console.log(ev);
+    };
+
+    const checkedItems = [1, 2, 3, 4];
+
     return (
         <>
             <div
@@ -26,60 +59,57 @@ const TestList = () => {
                 }}
             >
                 <List>
-                    <ListItem>
+                    <ListItem button selected={selectedIndex === 0} onClick={handleItemClick(0)}>
                         <ListItemIcon>
                             <SearchIcon size="medium" />
                         </ListItemIcon>
                         <ListItemText truncate flex>
                             Lorem ipsum dolor sit, amet consectetur
                         </ListItemText>
-                        <ListItemAction className="list__action">
-                            <IconButton size="small">
+                        <ListItemAction>
+                            <IconButton size="small" tabIndex="-1">
                                 <ClearCloseIcon />
                             </IconButton>
                         </ListItemAction>
                     </ListItem>
-                    <ListItem>
+                    <ListItem button selected={selectedIndex === 1} onClick={handleItemClick(1)}>
                         <ListItemIcon>
                             <SearchIcon size="medium" />
                         </ListItemIcon>
                         <ListItemText truncate flex>
                             Lorem ipsum dolor sit, amet consectetur
                         </ListItemText>
-                        <ListItemAction className="list__action">
-                            <IconButton size="small">
+                        <ListItemAction>
+                            <IconButton size="small" tabIndex="-1">
                                 <ClearCloseIcon />
                             </IconButton>
                         </ListItemAction>
                     </ListItem>
-                    <ListItem>
+                    <ListItem button selected={selectedIndex === 2} onClick={handleItemClick(2)}>
                         <ListItemIcon>
                             <SearchIcon size="medium" />
                         </ListItemIcon>
                         <ListItemText truncate flex>
                             Lorem ipsum dolor sit, amet consectetur
                         </ListItemText>
-                        <ListItemAction className="list__action">
-                            <IconButton size="small">
+                        <ListItemAction>
+                            <IconButton size="small" tabIndex="-1" onClick={handleDeleteClick}>
                                 <ClearCloseIcon />
                             </IconButton>
                         </ListItemAction>
                     </ListItem>
                     <Divider />
-                    <ListItem>
-                        <ListItemIcon>
-                            <Checkbox />
-                        </ListItemIcon>
-                        <ListItemText flex>Lorem ipsum dolor sit, amet consectetur</ListItemText>
-                        <ListItemText>10</ListItemText>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon>
-                            <Checkbox />
-                        </ListItemIcon>
-                        <ListItemText flex>Lorem ipsum dolor sit, amet consectetur</ListItemText>
-                        <ListItemText>10</ListItemText>
-                    </ListItem>
+                    {checkedItems.map((item) => (
+                        <ListItem key={item} button onClick={handleChecked(item)}>
+                            <ListItemAction>
+                                <Checkbox checked={checked.indexOf(item) !== -1} tabIndex="-1" />
+                            </ListItemAction>
+                            <ListItemText flex>
+                                Lorem ipsum dolor sit, amet consectetur
+                            </ListItemText>
+                            <ListItemText>(34)</ListItemText>
+                        </ListItem>
+                    ))}
                 </List>
             </div>
         </>
