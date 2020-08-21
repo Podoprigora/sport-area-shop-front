1562241ba235655226fa4a5a2ee17f1385413bd0
Alexandr Podoprigora <podoprigoraisv@gmail.com>
2020-08-21 11:53:28 +0300

    Updating packages

--- src/ui/SvgIcons/withSvgAttrStyles.js
+++ src/ui/SvgIcons/withSvgAttrStyles.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const withIconAttributes = (ComposedComponent) => (props) => {
-    const { className, size, primary, ...other } = props;
-    const sizeClass = size && `icon--${size}`;
+const withIconAttributes = (ComposedComponent) => {
+    const EnhancedComponent = (props) => {
+        const { className, size, primary, ...other } = props;
 
-    return (
-        <div
-            className={classNames('icon', sizeClass, className, {
-                'icon--primary': primary
-            })}
-            {...other}
-        >
-            <ComposedComponent preserveAspectRatio="xMidYMid meet" className="svg-icon" />
-        </div>
-    );
+        const sizeClass = size && `icon--${size}`;
+
+        return (
+            <div
+                className={classNames('icon', sizeClass, className, {
+                    'icon--primary': primary
+                })}
+                {...other}
+            >
+                <ComposedComponent preserveAspectRatio="xMidYMid meet" className="svg-icon" />
+            </div>
+        );
+    };
+
+    EnhancedComponent.propTypes = {
+        className: PropTypes.string,
+        size: PropTypes.string,
+        primary: PropTypes.bool
+    };
+
+    return EnhancedComponent;
+};
+
+withIconAttributes.propTypes = {
+    ComposedComponent: PropTypes.element
 };
 
 export default withIconAttributes;
