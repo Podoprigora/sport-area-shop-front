795c1f5f203fabea683097728f2634d1a21f2594
Alexandr Podoprigora <podoprigoraisv@gmail.com>
2020-08-29 13:11:52 +0300

    Refactoring markup of CatalogFiltersPriceForm

--- src/pages/CatalogPage/components/CatalogFilters/CatalogFiltersPrice/CatalogFiltersPriceForm.js
+++ src/pages/CatalogPage/components/CatalogFilters/CatalogFiltersPrice/CatalogFiltersPriceForm.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import FlexRow from '@ui/FlexRow';
-import FlexCol from '@ui/FlexCol';
 import NumberInput from '@ui/NumberInput';
 import Button from '@ui/Button';
 import Slider from '@ui/Slider';
@@ -10,20 +8,20 @@ import Slider from '@ui/Slider';
 const CatalogFiltersPriceForm = (props) => {
     return (
         <form className="catalog-page-filters-panel__form">
-            <FlexRow>
-                <FlexCol>
+            <div className="u-flex">
+                <div className="u-flex-item-1">
                     <NumberInput placeholder="from" simple fullWidth />
-                </FlexCol>
+                </div>
                 <div className="u-centered u-width-8 u-color-grey">--</div>
-                <FlexCol>
+                <div className="u-flex-item-1">
                     <NumberInput placeholder="to" simple fullWidth />
-                </FlexCol>
-                <FlexCol xs="auto" className="u-margin-l-6">
+                </div>
+                <div className="u-margin-l-6">
                     <Button primary centered slim>
                         OK
                     </Button>
-                </FlexCol>
-            </FlexRow>
+                </div>
+            </div>
             <div className="u-margin-y-2">
                 <Slider defaultValue={[0, 100]} disabledThumbLabel />
             </div>
