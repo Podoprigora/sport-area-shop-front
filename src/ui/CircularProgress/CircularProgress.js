832b7d3a51d39271a693ef69070a3b0429b26b55
Alexandr Podoprigora <podoprigoraisv@gmail.com>
2020-10-31 22:42:00 +0200

    Refactoring CircularProgress

--- src/ui/CircularProgress/CircularProgress.js
+++ src/ui/CircularProgress/CircularProgress.js
@@ -5,45 +5,17 @@ import classNames from 'classnames';
 const circleSize = 50;
 
 const CircularProgress = React.forwardRef(function CircularProgress(
-    {
-        size = 'medium',
-        conteinerSize: conteinerSizeProp,
-        strokeWidth,
-        primary,
-        secondary,
-        className,
-        style
-    },
+    { size = 'medium', primary, secondary, className, style },
     ref
 ) {
-    let containerSize = conteinerSizeProp;
-    let circleStrokeWidth = strokeWidth;
-
-    switch (size) {
-        case 'small':
-            containerSize = 24;
-            circleStrokeWidth = 4;
-            break;
-        case 'medium':
-            containerSize = 36;
-            circleStrokeWidth = 3;
-            break;
-        case 'large':
-            containerSize = 60;
-            circleStrokeWidth = 2.25;
-            break;
-        default:
-            containerSize = size;
-            circleStrokeWidth = strokeWidth;
-    }
-
     return (
         <div
             className={classNames('circular-progress', className, {
                 'circular-progress--primary': primary,
-                'circular-progress--secondary': secondary
+                'circular-progress--secondary': secondary,
+                [`circular-progress--${size}`]: size
             })}
-            style={{ ...style, width: containerSize, height: containerSize }}
+            style={style}
             ref={ref}
         >
             <svg viewBox={`0 0 ${circleSize} ${circleSize}`} className="circular-progress__svg">
@@ -52,7 +24,6 @@ const CircularProgress = React.forwardRef(function CircularProgress(
                     cx={circleSize / 2}
                     cy={circleSize / 2}
                     r={circleSize / 2 - 5}
-                    strokeWidth={circleStrokeWidth}
                 />
             </svg>
         </div>
@@ -60,7 +31,7 @@ const CircularProgress = React.forwardRef(function CircularProgress(
 });
 
 CircularProgress.propTypes = {
-    size: PropTypes.oneOf(['small', 'medium', 'large', null]),
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
     conteinerSize: PropTypes.number,
     strokeWidth: PropTypes.number,
     style: PropTypes.object,
