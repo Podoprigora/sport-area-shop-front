ff07fdb31310e055ff93691106f9c188aaa81ccd
Alexandr Podoprigora <podoprigoraisv@gmail.com>
2021-03-14 18:27:07 +0200

    TS integraion: ReactRouterScrollToTop

--- src/pa-ui-kit/components/ReactRouter/ReactRouterScrollToTop.tsx
+++ src/pa-ui-kit/components/ReactRouter/ReactRouterScrollToTop.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export default function ReactRouterScrollToTop() {
+export function ReactRouterScrollToTop() {
     const { pathname } = useLocation();
 
     useEffect(() => {
--- src/pa-ui-kit/components/ReactRouter/index.js
+++
@@ -1,1 +0,0 @@
-export { default as ReactRouterScrollToTop } from './ReactRouterScrollToTop';
